076cb98a198c6913ccba52c46489d0f555557d2d
ethriel3695 <ethriel3695@gmail.com>
2020-02-29 18:21:29 -0700

    Updated toml file again and removed unused

--- one-thousand-eyes/src/gatsby-theme-contentful/components/ListComponent.js
+++ one-thousand-eyes/src/gatsby-theme-contentful/components/ListComponent.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInbox } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles(theme => ({
   root: {
