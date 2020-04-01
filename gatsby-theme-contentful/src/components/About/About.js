7202ceb2d641c9e86f61e659a932bb4cf32a7f67
ethriel3695 <ethriel3695@gmail.com>
2020-04-01 17:33:48 -0600

    About section and testing for email, call, text

---
+++ gatsby-theme-contentful/src/components/About/About.js
@@ -0,0 +1,121 @@
+import React from 'react';
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+
+// @material-ui/icons
+// import PermIdentity from '@material-ui/icons/PermIdentity';
+
+// core components
+import { Grid } from '@material-ui/core';
+import GridItem from '../Grid/GridItem.js';
+import Button from '../CustomButtons/Button.js';
+import Card from '../Card/Card.js';
+import CardBody from '../Card/CardBody.js';
+import CardAvatar from '../Card/CardAvatar.js';
+
+import styles from '../../../assets/jss/material-dashboard-pro-react/views/userProfileStyles.js';
+
+import avatar from '../../../assets/img/faces/marc.jpg';
+
+const useStyles = makeStyles(styles);
+
+export default function About() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid container>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card profile>
+            <CardAvatar profile>
+              <a href="#pablo" onClick={e => e.preventDefault()}>
+                <img src={avatar} alt="..." />
+              </a>
+            </CardAvatar>
+            <CardBody profile>
+              <h6 className={classes.cardCategory}>CTO / TECH-FOUNDER</h6>
+              <h4 className={classes.cardTitle}>Samantha Stavast</h4>
+              <p className={classes.description}>
+                Don{"'"}t be scared of the truth because we need to restart the
+                human foundation in truth And I love you like Kanye loves Kanye!
+              </p>
+              <Button color="primary" round>
+                <a
+                  style={{ textDecoration: 'none', color: '#eee' }}
+                  href="mailto:ethriel3695@gmail.com"
+                >
+                  Email
+                </a>
+              </Button>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card profile>
+            <CardAvatar profile>
+              <a href="#pablo" onClick={e => e.preventDefault()}>
+                <img src={avatar} alt="..." />
+              </a>
+            </CardAvatar>
+            <CardBody profile>
+              <h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>
+              <h4 className={classes.cardTitle}>John Stavast</h4>
+              <p className={classes.description}>
+                Don{"'"}t be scared of the truth because we need to restart the
+                human foundation in truth And I love you like Kanye loves Kanye!
+              </p>
+              <Button color="linkedin" round>
+                <a
+                  style={{ textDecoration: 'none', color: '#eee' }}
+                  href="mailto:ethriel3695@gmail.com"
+                >
+                  Email
+                </a>
+              </Button>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card profile>
+            <CardAvatar profile>
+              <a href="#pablo" onClick={e => e.preventDefault()}>
+                <img src={avatar} alt="..." />
+              </a>
+            </CardAvatar>
+            <CardBody profile>
+              <h6 className={classes.cardCategory}>CTO / TECH-FOUNDER</h6>
+              <h4 className={classes.cardTitle}>Reuben Ellis</h4>
+              <p className={classes.description}>
+                Don{"'"}t be scared of the truth because we need to restart the
+                human foundation in truth And I love you like Kanye loves Kanye!
+              </p>
+              <Button color="github" round>
+                <a
+                  style={{ textDecoration: 'none', color: '#eee' }}
+                  href="mailto:ethriel3695@gmail.com"
+                >
+                  Email
+                </a>
+              </Button>
+              <Button color="github" round>
+                <a
+                  style={{ textDecoration: 'none', color: '#eee' }}
+                  href="tel:12085697538"
+                >
+                  Call
+                </a>
+              </Button>
+              <Button color="github" round>
+                <a
+                  style={{ textDecoration: 'none', color: '#eee' }}
+                  href="sms://+12085697538?body=What%20Can%20I%20Assist%20You%20With?"
+                >
+                  Text
+                </a>
+              </Button>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </Grid>
+    </div>
+  );
+}
--- gatsby-theme-contentful/src/components/Grid/GridContainer.js
+++ gatsby-theme-contentful/src/components/Grid/GridContainer.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const styles = {
   grid: {
-    margin: "0 -15px",
-    width: "calc(100% + 30px)"
+    margin: '0 -15px',
+    width: 'calc(100% + 30px)',
     // '&:before,&:after':{
     //   display: 'table',
     //   content: '" "',
@@ -16,7 +16,7 @@ const styles = {
     // '&:after':{
     //   clear: 'both',
     // }
-  }
+  },
 };
 
 const useStyles = makeStyles(styles);
@@ -25,7 +25,7 @@ export default function GridContainer(props) {
   const classes = useStyles();
   const { children, className, ...rest } = props;
   return (
-    <Grid container {...rest} className={classes.grid + " " + className}>
+    <Grid container {...rest} className={classes.grid + ' ' + className}>
       {children}
     </Grid>
   );
@@ -33,5 +33,5 @@ export default function GridContainer(props) {
 
 GridContainer.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node
+  children: PropTypes.node,
 };
