28b7937648795afb43dcc52c470d046cfac7f490
ethriel3695 <ethriel3695@gmail.com>
2020-04-14 06:49:25 -0600

    Added logic for browser API check

--- gatsby-theme-contentful/src/components/layout.js
+++ gatsby-theme-contentful/src/components/layout.js
@@ -5,27 +5,30 @@ import './layout.css';
 import Header from './Header/Header';
 import Footer from './Footer';
 
-Notification.requestPermission(function(status) {
-  console.log('Notification permission status:', status);
-});
+const isBrowser = typeof window !== 'undefined';
+if (isBrowser) {
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+  });
 
-function displayNotification() {
-  if (Notification.permission == 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      var options = {
-        body: 'Here is a notification body!',
-        icon: 'images/example.png',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: 1,
-        },
-      };
-      reg.showNotification('Hello world!', options);
-    });
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        var options = {
+          body: 'Here is a notification body!',
+          icon: 'images/example.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1,
+          },
+        };
+        reg.showNotification('Hello world!', options);
+      });
+    }
   }
+  displayNotification();
 }
-displayNotification();
 
 const Layout = ({ children, ...props }) => {
   return (
