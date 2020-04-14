817f8bf60765efdbc13d0f3f2dbf5e4e6f3ab7ef
ethriel3695 <ethriel3695@gmail.com>
2020-04-14 06:40:12 -0600

    Test push notifications

--- gatsby-theme-contentful/src/components/layout.js
+++ gatsby-theme-contentful/src/components/layout.js
@@ -5,6 +5,28 @@ import './layout.css';
 import Header from './Header/Header';
 import Footer from './Footer';
 
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1,
+        },
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
+displayNotification();
+
 const Layout = ({ children, ...props }) => {
   return (
     <StyledLayout>
