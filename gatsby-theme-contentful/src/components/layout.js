257f038afec021a3fdd6d211e7cde1f7feaad86e
ethriel3695 <ethriel3695@gmail.com>
2020-04-14 06:55:14 -0600

    Remove icon which is failing

--- gatsby-theme-contentful/src/components/layout.js
+++ gatsby-theme-contentful/src/components/layout.js
@@ -16,7 +16,6 @@ if (isBrowser) {
       navigator.serviceWorker.getRegistration().then(function(reg) {
         var options = {
           body: 'Here is a notification body!',
-          icon: 'images/example.png',
           vibrate: [100, 50, 100],
           data: {
             dateOfArrival: Date.now(),
