be72446f3691ec9566b5f7d83ac613622691a26c
ethriel3695 <ethriel3695@gmail.com>
2020-06-19 19:54:38 -0600

    update class to className

--- gatsby-theme-contentful/src/components/layout.js
+++ gatsby-theme-contentful/src/components/layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children, ...props }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header {...props} />
-      <main class="flex-grow">{children}</main>
+      <main className="flex-grow">{children}</main>
       <hr
         className="mt-12 border-top-2;
 "
