5060da01b98af9ee528ee4f525c2ca734edf65f2
ethriel3695 <ethriel3695@gmail.com>
2020-11-02 23:10:17 -0700

    Cleaned up files and updated dependencies

--- demo/src/components/Mentors.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Mentors = ({ children, ...props }) => {
-  return (
-    <div className="flex flex-col min-h-screen">Team Creation is Awesome!</div>
-  );
-};
-
-export default Mentors;
